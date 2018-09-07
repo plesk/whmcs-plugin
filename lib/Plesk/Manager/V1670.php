b729b77d932be692a95d4246554a8926edcbed8e
Nate Custer <nate@whmcs.com>
2018-09-07 07:51:17 -0500

    Implement SSL Install Support for Plesk 17.8+ CORE-12440

---
+++ lib/Plesk/Manager/V1670.php
@@ -0,0 +1,25 @@
+<?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH.
+// @codingStandardsIgnoreLine
+class Plesk_Manager_V1670 extends Plesk_Manager_V1660
+{
+    /**
+     * @param $params
+     * @return string
+     */
+    // @codingStandardsIgnoreLine
+    protected function _generateCSR($params)
+    {
+        return parent::_generateCSR($params);
+    }
+
+    /**
+     * @param $params
+     * @return string
+     */
+    // @codingStandardsIgnoreLine
+    protected function _installSsl($params)
+    {
+        return parent::_installSsl($params);
+    }
+}
